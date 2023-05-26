5e253c5951e759e9b35494982a346a74ead13cab
Harshit <harshitupadhyay872@gmail.com>
2023-05-26 11:22:40 +0530

    Updates

---
+++ precache-manifest.7ab6e886832c7f21a73c177d0e625779.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "64a811de6ee673857c8b",
+    "url": "/addy-mobile28/static/css/main.d5835a07.chunk.css"
+  },
+  {
+    "revision": "64a811de6ee673857c8b",
+    "url": "/addy-mobile28/static/js/main.64a811de.chunk.js"
+  },
+  {
+    "revision": "bfa17f515437437af62a",
+    "url": "/addy-mobile28/static/js/1.bfa17f51.chunk.js"
+  },
+  {
+    "revision": "31807d27aa84233a7cf3",
+    "url": "/addy-mobile28/static/css/2.7523488e.chunk.css"
+  },
+  {
+    "revision": "31807d27aa84233a7cf3",
+    "url": "/addy-mobile28/static/js/2.31807d27.chunk.js"
+  },
+  {
+    "revision": "107b98c3420ff3d3d0e4",
+    "url": "/addy-mobile28/static/js/runtime~main.107b98c3.js"
+  },
+  {
+    "revision": "b6316a6cefc98d97905c4c41e18d9a9a",
+    "url": "/addy-mobile28/static/media/deposit.b6316a6c.png"
+  },
+  {
+    "revision": "d9ada458a4e77bf7212b3adc39ab2805",
+    "url": "/addy-mobile28/static/media/transfer.d9ada458.png"
+  },
+  {
+    "revision": "f1b4270706c9a3d380ab7c082ac45925",
+    "url": "/addy-mobile28/static/media/withdraw.f1b42707.png"
+  },
+  {
+    "revision": "c145454fb0b9c1334db588c83be7d4cb",
+    "url": "/addy-mobile28/static/media/sidepush-header1.c145454f.jpg"
+  },
+  {
+    "revision": "c0fd57ad04f634aa7a87a2a3cd5b1faa",
+    "url": "/addy-mobile28/static/media/logo.c0fd57ad.png"
+  },
+  {
+    "revision": "ae0bfa82f983cf1832c7038bac0fb9c5",
+    "url": "/addy-mobile28/static/media/auto-ewallet.ae0bfa82.jpg"
+  },
+  {
+    "revision": "a2465140fa88edf31673a01e47788710",
+    "url": "/addy-mobile28/static/media/online.a2465140.jpg"
+  },
+  {
+    "revision": "4b0662c4c4bd847e9aca903917f387fa",
+    "url": "/addy-mobile28/static/media/ATM.4b0662c4.jpg"
+  },
+  {
+    "revision": "10e3e01851770ef29ba1f21a9d7d2d79",
+    "url": "/addy-mobile28/index.html"
+  }
+];
